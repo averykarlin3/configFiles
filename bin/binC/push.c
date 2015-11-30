d5c3fbd5f2b20e0e4bd3780e4e3698b6f33fa833
spocklogic <averykarlin3@gmail.com>
2015-11-30 13:44:32 -0500

    "Update"

--- bin/binC/push.c
+++ bin/binC/push.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 int execute(char** command) {
 	pid_t chP = fork();
@@ -17,15 +18,13 @@ int execute(char** command) {
 }
 
 int main(int argc, char *argv[]) {
-	char** add = {"git", "add", "*"};
-	char** commit = {"git", "commit", "-a", "-m", "\"Update\""};
-	char** push = {"git", "push"};
-	if(argc == 1 && !strcmp(argv[1], "-a")) {
+	char* add[] = {"git", "add", "*"};
+	char* commit[] = {"git", "commit", "-a", "-m", "\"Update\""};
+	char* push[] = {"git", "push"};
+	if(argc == 2 && !strcmp(argv[1], "-a")) {
 		execute(add);
-		execute(commit);
-		execute(push);
 	}
-	else if(argc == 0) {
+	else if(argc == 1) {
 		execute(commit);
 		execute(push);
 	}
