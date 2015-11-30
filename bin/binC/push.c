bf79d223584570d04d5b1b9dd31977a6b6175716
spocklogic <averykarlin3@gmail.com>
2015-11-30 14:02:21 -0500

    Update

--- bin/binC/push.c
+++ bin/binC/push.c
@@ -18,20 +18,28 @@ int execute(char** command) {
 }
 
 int main(int argc, char *argv[]) {
-	char* add[] = {"git", "add", "*"};
-	char* commit[] = {"git", "commit", "-a", "-m", "\"Update\""};
-	char* push[] = {"git", "push"};
-	printf("%i", argc);
-	if(argc == 2 && strcmp(argv[1], "-a") == 0) {
-		execute(add);
+	char* add[3] = {"git", "add", "*"};
+	char* commit[5] = {"git", "commit", "-a", "-m", "\"Update\""};
+	char* push[2] = {"git", "push"};
+	if(argc > 1) {
+		int n = 1;
+		while(n < argc) {
+			if(!strcmp(argv[n], "-a")) {
+				execute(add);
+			}
+			else if(!strcmp(argv[n], "-m")) {
+				char* commit[5] = {"git", "commit", "-a", "-m", argv[++n]};
+			}
+			else if(!strcmp(argv[n], "-f")) {
+				char* push[3] = {"git", "push", "-f"};
+			}
+			else {
+				printf("Incorrect command\n");
+				return -1;
+			}
+		}
 	}
-	else if(argc == 1) {
-		execute(commit);
-		printf("Done");
-		execute(push);
-	}
-	else {
-		printf("Error: Unknown Prompt");
-	}
+	execute(commit);
+	execute(push);
 	return 0;
 }
