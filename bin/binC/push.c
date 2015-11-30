5536024a0a398774b0901073de993cc087f1a8e7
spocklogic <averykarlin3@gmail.com>
2015-11-30 13:50:43 -0500

    "Update"

--- bin/binC/push.c
+++ bin/binC/push.c
@@ -21,11 +21,11 @@ int main(int argc, char *argv[]) {
 	char* add[] = {"git", "add", "*"};
 	char* commit[] = {"git", "commit", "-a", "-m", "\"Update\""};
 	char* push[] = {"git", "push"};
+	printf("%i", argc);
 	if(argc == 2 && strcmp(argv[1], "-a") == 0) {
 		execute(add);
 	}
 	else if(argc == 1) {
-		printf(argv[0]);
 		execute(commit);
 		execute(push);
 	}
